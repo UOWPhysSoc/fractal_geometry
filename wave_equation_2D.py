5d17632047d314203d2bba2c056c650cac0f6141
James Archer <james__archer@hotmail.com>
2014-08-20 19:23:22 +1000

    Cleaned up, made less ambiguous variable names

--- wave_equation_2D.py
+++ wave_equation_2D.py
@@ -1,7 +1,7 @@
 from visual import *
 from math import *
 
-position = []
+height = []
 vel = []
 
 #Number of position subdivisions
@@ -15,15 +15,17 @@ dt = 0.1
 
 #Generate initial distribution
 for n in range(0,N):
-    position.append([])
+    height.append([])
     for m in range(0,N):
-        position[n].append(5*exp(-0.05*((n - N/2)**2 + (m - N/2)**2)))
+        height[n].append(5*exp(-0.05*((n - N/2)**2 + (m - N/2)**2)))
         vel.append(0)
+        
+#Fix edges to zero
 for n in range(0,N):
-    position[0][n] = 0
-    position[n][0] = 0
-    position[N-1][n] = 0
-    position[n][N-1] = 0
+    height[0][n] = 0
+    height[n][0] = 0
+    height[N-1][n] = 0
+    height[n][N-1] = 0
 
 
 #List of point objects
@@ -33,7 +35,7 @@ p = []
 for i in range(0,N):
     p.append([])
     for j in range(0,N):
-        x = vector(i-N/2, j - N/2, position[i][j]) #vpython is being a bitch and fucking up the pos object somehow
+        x = vector(i-N/2, j - N/2, height[i][j])
         p[i].append(points(pos = x, vel = vel[i]))
 
 
@@ -45,7 +47,7 @@ while True:
         for m in range(1,N-1): # iterate over y
             acc_x = (p[n][m+1].pos[0][2] - 2*p[n][m].pos[0][2] + p[n][m-1].pos[0][2])/dx**2
             acc_y = (p[n+1][m].pos[0][2] - 2*p[n][m].pos[0][2] + p[n-1][m].pos[0][2])/dx**2             
-            p[n][m].vel += (acc_x + acc_y)*dt
+            p[n][m].vel += c**2 * (acc_x + acc_y)*dt
 
     #Update positions
     for n in range(1,N-1):
