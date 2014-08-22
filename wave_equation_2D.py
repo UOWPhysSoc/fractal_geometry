8502cf3ee77c01ddf8ee1738b84e7e439624268e
James Archer <james__archer@hotmail.com>
2014-08-22 20:08:30 +1000

    Allows free boundaries
    Changing the bool “fixed_edges” allows the boundaries to be free or
    fixed, and automatically couples the boundary with the opposite to
    allow a wave to propagate across one side and out the other.

--- wave_equation_2D.py
+++ wave_equation_2D.py
@@ -4,28 +4,37 @@ from math import *
 height = []
 vel = []
 
+fixed_ends = False
+
 #Number of position subdivisions
 N = 20
 #Speed of wave
-c = 1
+c = 0.5
 #Position, time subdivisions (position divisions are redundant at this stage)
 dx = 1
-dt = 0.1
+dt = 0.05
 
+if fixed_ends == True:
+    A = 1
+    B = N-1
+else:
+    A = 0
+    B = N
 
 #Generate initial distribution
 for n in range(0,N):
     height.append([])
     for m in range(0,N):
-        height[n].append(5*exp(-0.05*((n - N/2)**2 + (m - N/2)**2)))
+        height[n].append(3*exp(-0.1*((n - N/2)**2 + (m - N/2)**2)))
         vel.append(0)
         
 #Fix edges to zero
-for n in range(0,N):
-    height[0][n] = 0
-    height[n][0] = 0
-    height[N-1][n] = 0
-    height[n][N-1] = 0
+if fixed_ends == True:
+    for n in range(0,N):
+        height[0][n] = 0
+        height[n][0] = 0
+        height[N-1][n] = 0
+        height[n][N-1] = 0
 
 
 #List of point objects
@@ -40,16 +49,19 @@ for i in range(0,N):
 
 
 #Loop the wavyness
+
+sleep(2)
 while True:
     rate(100)
     #Update acceleration and velocity all at once
-    for n in range(1,N-1):  # iterate over x
-        for m in range(1,N-1): # iterate over y
-            acc_x = (p[n][m+1].pos[0][2] - 2*p[n][m].pos[0][2] + p[n][m-1].pos[0][2])/dx**2
-            acc_y = (p[n+1][m].pos[0][2] - 2*p[n][m].pos[0][2] + p[n-1][m].pos[0][2])/dx**2             
+   
+    for n in range(A,B):  # iterate over x
+        for m in range(A,B): # iterate over y
+            acc_x = (p[n][(m+1)%N].pos[0][2] - 2*p[n][m].pos[0][2] + p[n][(m-1)%N].pos[0][2])/dx**2
+            acc_y = (p[(n+1)%N][m].pos[0][2] - 2*p[n][m].pos[0][2] + p[(n-1)%N][m].pos[0][2])/dx**2             
             p[n][m].vel += c**2 * (acc_x + acc_y)*dt
 
     #Update positions
-    for n in range(1,N-1):
-        for m in range(1,N-1):
+    for n in range(A,B):
+        for m in range(A,B):
             p[n][m].pos[0][2] += p[n][m].vel*dt
